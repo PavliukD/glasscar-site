759cb9ee3a1cd785fc6fae5fd20ec0136619e6bf
PavliukD <albatrozzz1@gmail.com>
2023-10-22 22:22:04 +0200

    pages

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require('path');
 
-let htmlPageNames = ['404', 'services', 'repair', 'install', 'about-us', 'sertificats'];
+let htmlPageNames = ['404', 'services', 'repair', 'install', 'about-us', 'sertificats', 'manufacturers'];
 let multipleHtmlPlugins = htmlPageNames.map(name => {
   return new HtmlWebpackPlugin({
     template: `./src/${name}.html`, // relative path to the HTML files
